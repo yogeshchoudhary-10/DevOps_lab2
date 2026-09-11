00a123c3841362423d8f8a13935a75f8e4901181
yogeshchoudhary-10 <choudharyyogesh219@gmail.com>
2026-09-11 15:58:20 +0530

    edit folder

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -1,6 +1,8 @@
 package hello;
+
 public class Hello {
- public static void main(String[] args) {
- System.out.println("Hello from Jenkins Pipeline CI/CD!");
- }
+  public static void main(String[] args) {
+    System.out.println("Hello from Jenkins Pipeline CI/CD!");
+    System.out.println("Hello from Yogesh!");
+  }
 }
